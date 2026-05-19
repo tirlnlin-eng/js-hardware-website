8ef30374e661c0bd0374371f7964424a5e23c419
tirlnlin-eng <tirlnlin@gmail.com>
2026-05-19 13:42:40 +0800

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,1 +1,11 @@
-
+export default function ContactPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>聯絡我們</h1>
+      <p>鉅晟電料五金行</p>
+      <p>地址：桃園市桃園區大華九街6號</p>
+      <p>電話：03-332-0958</p>
+      <p>Email：JS3320958@gmail.com</p>
+    </main>
+  )
+}
